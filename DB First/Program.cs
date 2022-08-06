43635ddf816114ef72d06ed8a1918c488d0ce7d7
HkhCode <hkh.2003.1381@gmail.com>
2022-08-06 11:48:08 -0700

    Files Upleaded

---
+++ DB First/Program.cs
@@ -0,0 +1,33 @@
+﻿using DB_First.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new UnivercityEntities())
+            {
+                var city = context.Cities.FirstOrDefault(x => x.Id == 1);
+                Console.WriteLine(city.Id);
+                Console.WriteLine(city.Name);
+                Console.WriteLine("====================");
+                context.Cities.Add(new City
+                {
+                    Name = "Tehran"
+                }
+                );
+                context.SaveChanges();
+                var city_selected = context.Cities.FirstOrDefault(x => x.Name == "Tehran");
+                Console.WriteLine(city_selected.Name);
+                Console.WriteLine("===============");
+                Console.ReadKey();
+            }
+        }
+    }
+}
